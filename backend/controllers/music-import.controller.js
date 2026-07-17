3e921e9a2e49bbc1b6dd668a0b452254c86a5d01
sergiovian <serginho.vian@gmail.com>
2026-07-17 15:27:34 -0300

    fix: verificar duplicidades durante upload JSON

--- backend/controllers/music-import.controller.js
+++ backend/controllers/music-import.controller.js
@@ -1,5 +1,5 @@
 const MusicImportService=require('../services/music-import.service');
 const { runChurchContext } = require('../constants/church-context');
-async function preview(req,res){const data=await runChurchContext(req.user.churchId,()=>MusicImportService.preview(req.files));res.status(200).json({data});}
+async function preview(req,res){const data=await MusicImportService.preview(req.files,req.user.churchId);res.status(200).json({data});}
 async function confirm(req,res){const data=await runChurchContext(req.user.churchId,()=>MusicImportService.importSongs(req.body.songs));res.status(201).json({data});}
 module.exports={preview,confirm};
